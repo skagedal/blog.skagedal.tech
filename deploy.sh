ce4205498b3f19e4efc5e91d0da0cc11e417ca95
Simon Kågedal Reimer <skagedal@gmail.com>
2023-02-06 10:24:10 +0100

    Improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-bundle install --quiet || exit 1
+if [[ "$1" == "--verbose" ]]; then
+    BUNDLE_INSTALL_ARG=""
+    RSYNC_ARG="--verbose"
+else
+    BUNDLE_INSTALL_ARG="--quiet"
+    RSYNC_ARG=""
+fi
+
+echo "💁 Installing Jekyll dependencies..."
+bundle install $BUNDLE_INSTALL_ARG || exit 1
+
+echo "💁 Generating site with Jekyll..."
 bundle exec jekyll build || exit 1
 
+echo "💁 Uploading..."
 rsync \
     --archive \
     --compress \
-    --progress \
     --delete \
-    --verbose \
+    --info=progress2 \
+    $RSYNC_ARG \
     _site/ \
     simon@skagedal.tech:blog
 
+echo "💁 Done!"
