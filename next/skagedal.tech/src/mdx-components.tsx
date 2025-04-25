937b825bfc1e67446dd30f6a49992444995503d3
Simon Kågedal Reimer <skagedal@gmail.com>
2025-04-25 22:44:51 +0200

    rm next.js experiment

--- next/skagedal.tech/src/mdx-components.tsx
+++
@@ -1,9 +0,0 @@
-import type { MDXComponents } from "mdx/types";
-
-// https://nextjs.org/docs/app/api-reference/file-conventions/mdx-components
-
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return {
-    ...components,
-  };
-}
