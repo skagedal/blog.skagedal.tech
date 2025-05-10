fe210724f01ab446d66ef734d3300e5940c1f844
Simon Kågedal Reimer <skagedal@gmail.com>
2025-05-10 14:01:34 +0200

    final fixes to next version

--- blogdans/src/main/java/skagedal/blogdans/render/Renderer.java
+++ blogdans/src/main/java/skagedal/blogdans/render/Renderer.java
@@ -2,7 +2,6 @@ package skagedal.blogdans.render;
 
 import j2html.rendering.FlatHtml;
 import j2html.tags.DomContent;
-import j2html.tags.specialized.ArticleTag;
 import j2html.tags.specialized.DivTag;
 import j2html.tags.specialized.HtmlTag;
 import j2html.tags.specialized.MetaTag;
@@ -13,10 +12,12 @@ import skagedal.blogdans.domain.Post;
 import skagedal.blogdans.domain.Site;
 import skagedal.blogdans.domain.User;
 import skagedal.blogdans.jekyll.SiteContext;
+import skagedal.blogdans.markdown.MarkdownRenderer;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import static j2html.TagCreator.*;
 
@@ -24,6 +25,7 @@ import static j2html.TagCreator.*;
 public class Renderer {
 
     private final Site site;
+    private final MarkdownRenderer markdownRenderer = new MarkdownRenderer();
 
     public Renderer(final Site site) {
         this.site = site;
@@ -62,32 +64,46 @@ public class Renderer {
         );
     }
 
-    private static DomContent postOverview(final List<Map<String, Object>> posts) {
+    private DomContent postOverview(final List<Map<String, Object>> posts) {
         return wrappedContent(div().withClasses("home")
             .with(
                 h1("Posts").withClasses("page-heading"),
-                postOverviewList(posts)
+                postOverviewList(posts),
+                subscribeViaRss()
             ));
     }
 
-    private static DomContent postOverviewList(final List<Map<String, Object>> posts) {
+    private DomContent subscribeViaRss() {
+        return p()
+            .withClasses("rss-subscribe")
+            .withText("subscribe ")
+            .with(
+                a().withHref(site.baseUri().resolve("/feed.xml").toString())
+                    .withText("via RSS")
+            );
+    }
+
+    private DomContent postOverviewList(final List<Map<String, Object>> posts) {
         return ul().withClasses("post-list")
             .with(
                 posts.stream()
-                    .map(Renderer::postInPostOverview)
+                    .map(this::postInPostOverview)
                     .toList()
             );
     }
 
-    private static DomContent postInPostOverview(final Map<String, Object> post) {
+    private DomContent postInPostOverview(final Map<String, Object> post) {
         final var title = (String) post.get("title");
         final var url = (String) post.get("url");
+        final var summary = Optional.ofNullable((String) post.get("summary"))
+            .map(markdownRenderer::render)
+            .orElse("");
         return li().with(
             span().withClasses("post-meta").withText((String) post.get("date")),
             h2(
                 a().withClasses("post-link")
                     .withHref(url).withText(title)),
-            p()
+            rawHtml(summary)
         );
     }
 
