003db98a7718abeb8742e1f3b275afff90a6e1ff
Simon Kågedal Reimer <skagedal@gmail.com>
2025-05-10 11:36:37 +0200

    report success to slack

---
+++ ci/report-success.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+person=$(git log -n1 --format='format:%aN' | awk '{print $1}')
+commithash=$(git log -n1 --format='format:%H')
+commiturl="https://github.com/skagedal/blogdans/commit/${commithash}"
+commit="<${commiturl}|commit>"
+curl -X POST -H 'Content-type: application/json' \
+  --data "{\"text\":\"Deployed blogdans from ${person}'s latest ${commit}.\"}" \
+  "${SLACK_WEBHOOK_URL}"
