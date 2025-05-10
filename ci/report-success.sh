8bd67464f2b9bf109f373a9da571a1242b61de8a
Simon Kågedal Reimer <skagedal@gmail.com>
2025-05-10 11:42:21 +0200

    fix url

--- ci/report-success.sh
+++ ci/report-success.sh
@@ -2,7 +2,7 @@
 
 person=$(git log -n1 --format='format:%aN' | awk '{print $1}')
 commithash=$(git log -n1 --format='format:%H')
-commiturl="https://github.com/skagedal/blogdans/commit/${commithash}"
+commiturl="https://github.com/skagedal/blog.skagedal.tech/commit/${commithash}"
 commit="<${commiturl}|commit>"
 curl -X POST -H 'Content-type: application/json' \
   --data "{\"text\":\"Deployed blogdans from ${person}'s latest ${commit}.\"}" \
