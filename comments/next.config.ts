a0a93f7e57f6638b33e98a0af645dc505075749f
Simon Kågedal Reimer <skagedal@gmail.com>
2025-05-18 09:35:41 +0200

    typo

--- comments/next.config.ts
+++ comments/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // https://nextjs.org/docs/pages/api-reference/config/next-config-js/output
   output: "standalone",
 };
 
